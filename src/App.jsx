4b2131aaba843097978ab9a96849161e8beed95a
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-17 20:00:16 -0300

    Fix styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import FAQ from "./components/Faq";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-16">
-      <FAQ />
+    <div className="flex items-center justify-center min-h-screen py-16">
+      <div className="flex flex-col items-center justify-center w-full">
+        <FAQ />
+      </div>
     </div>
   );
 }
